38593e427fb0623df49477643db7fa33b94ce1be
Ingvald Lorentzen <ingvald.lorentzen@intility.no>
2021-04-14 15:57:58 +0200

    - Conditionally imports Celery MetroidTask

--- metroid/__init__.py
+++ metroid/__init__.py
@@ -1,4 +1,7 @@
-from metroid.celery import MetroidTask  # noqa F401
+from metroid.config import settings
+
+if settings.worker_type == 'celery':
+    from metroid.celery import MetroidTask  # noqa F401
 from metroid.publish import publish_event  # noqa F401
 
 __version__ = '1.1.0'
